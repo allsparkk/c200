29564b799f14d6a58619ecf0361bfc333d5ee705
allsparkk <84027766+allsparkk@users.noreply.github.com>
2021-05-21 20:35:10 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c200
+# collisionAlgorithm
+Algorithm to detect collision between two objects
---
+++ sketch.js
@@ -0,0 +1,60 @@
+
+var fixedRect, movingRect;
+var gameObject1, gameObject2;
+
+function setup() {
+  createCanvas(1200,800);
+ 
+ 
+  movingRect = createSprite(800, 400,80,30);
+  movingRect.shapeColor = "green";
+
+  fixedRect = createSprite(100, 100, 50, 50);
+  fixedRect.shapeColor = "green";
+  
+  fixedRect2 = createSprite(200,50,50,50)
+  
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching(fixedRect,movingRect)){
+    movingRect.shapeColor = "blue";
+    fixedRect.shapeColor = "blue";
+  }
+
+  else if(isTouching(fixedRect2,movingRect)){
+    movingRect.shapeColor="blue"
+    fixedRect2.shapeColor="blue"
+  }
+
+  else {
+    movingRect.shapeColor = "green";
+  
+    fixedRect.shapeColor = "green"
+
+    fixedRect2.shapeColor="green"
+  }
+ 
+
+//bounceOff(movingRect,fixedRect)
+  drawSprites();
+}
+
+function isTouching(fixedRect,movingRect){
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+    && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
+    && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
+    
+    return true;
+  }
+  else {
+    return false;
+  } 
+}
+
+
